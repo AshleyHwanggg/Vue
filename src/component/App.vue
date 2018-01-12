b35d00b36903b16ef0ca42de39c33161f76b0ebe
hwang <416866636@qq.com>
2018-01-12 16:32:34 +0800

    环境测试

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+      测试，test test
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+//1.引入vue
 import Vue from 'vue';
+
+//2.引入根组件
+import App from './component/App.vue';
+
+
+new Vue({
+  el:"#app",
+  render:(creatElement=>creatElement(App))
+});
