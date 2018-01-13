bea36d27d87b5e0ec3fe556f03229116b425c03b
AshleyHwanggg <hwanggg@163.com>
2018-01-13 10:44:01 +0800

    项目初始化集成

--- src/component/App.vue
+++ src/component/App.vue
@@ -2,17 +2,17 @@
   <div>
       <!-- 测试，test test -->
       <router-view></router-view>
-       <!-- <el-rate
+       <el-rate
         v-model="value"
         :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
-       </el-rate> -->
+       </el-rate>
 
-       <el-rate
+       <!-- <el-rate
         v-model="value4"
         :icon-classes="['icon-rate-face-1', 'icon-rate-face-2', 'icon-rate-face-3']"
          void-icon-class="icon-rate-face-off"
         :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
-      </el-rate>
+      </el-rate> -->
   </div>
 </template>
 
@@ -20,10 +20,9 @@
   export default {
     data(){
       return{
-        // value:2,
-        value4:2 
-      },
-      icon-classes:['el-icon-star-on', 'el-icon-star-on','el-icon-star-on']
+        value:2
+        // value4:2 
+      }
       
     }
   }
